8c4a6b7d2f0b11064957700683bb84a4856190f0
james <hippo70328@gmail.com>
2018-11-02 01:31:51 +0800

    Add load api test for login and not login

--- twk_backend/twk_send/tests.py
+++ twk_backend/twk_send/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory, TestCase, Client
 
-# Create your tests here.
+from twk_auth.views import login
+
+from twk_load.views import load_hw
+
+from twk_save.views import save_hw
+
