8bb7f4542183403b4e49fee9912473e792502838
james <hippo70328@gmail.com>
2018-11-02 02:39:35 +0800

    refactor submit api test

--- twk_backend/twk_submit/tests.py
+++ twk_backend/twk_submit/tests.py
@@ -11,33 +11,36 @@ from twk_auth.views import login
 
 from twk_load.views import load_hw
 
+from twk_save.views import save_publish_hw
+
+from .views import submit_hw
+
 import json
 
+"""
+this test should open judge 0 server and set environment variable first, or it would be failed
+"""
 class twk_submit_test(TestCase):
     def setUp(self):
-        # Every test needs access to the request factory.
         self.client = Client()
         self.factory = RequestFactory()
         self.user = User.objects.create(
             username='test', password='password')
 
     def test_details(self):
-        # Create an instance of a GET request.
-        c = Client()
-        data = { 'question': 'question1', 'stdin': '1 1', 'stdout':'2', 'language_id':'4'}
-        response = self.client.post('/publish_hw/', data = json.dumps(data), content_type='application/json')
+        publish_data = { "question": "question1", "stdin": "1 1", "stdout":"2", "language_id":"4" }
+        request = self.factory.post('/publish_hw/', json.dumps(publish_data), content_type='application/json')
 
-        data = {'source_code':'printf("Hello World!")', 'language_id': '4', 'assignment_id': '1'}
-        request = self.factory.post('/load_hw/', data, format = 'json')
+        save_publish_hw(request)
 
+        submit_data = { 'source_code': "test", "language_id": "4", "assignment_id": "1"}
+        request = self.factory.post('/submit_hw/', json.dumps(submit_data), content_type='application/json')
         request.user = self.user
+        response = submit_hw(request)
 
-        
-        # Test my_view() as if it were deployed at /customer/details
-        print("12313132")
-        print(request)
-        print("1231313")
-        
-        # Use this syntax for class-based views.
-        # response = MyView.as_view()(request)
-        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
+
+        request.user = AnonymousUser()
+        response = submit_hw(request)
+
+        self.assertEqual(response.status_code, 302)
