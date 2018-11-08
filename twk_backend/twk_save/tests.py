31138ab1e13f917c68ced9e644cbd06bed4aadc0
james0828 <33977019+james0828@users.noreply.github.com>
2018-11-08 22:23:36 +0800

    Save hw test (#9)
    * test fix - login before calling publish_hw api
    
    * add test for save_hw by get and post method
    
    * add test for send review, get_code
    
    * test fix - login before calling publish_hw api
    
    * remove unused code
    
    * Add rocketchat to twkide dependency
    
    * add test for save_hw by get and post method
    
    * add test for send review, get_code
    
    * remove unused code
    
    * Add rocketchat to twkide dependency

--- twk_backend/twk_save/tests.py
+++ twk_backend/twk_save/tests.py
@@ -1,1 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+
+from .views import save_publish_hw, load_publish_hw
+from django.contrib.auth.models import User
+
+import json
+
+class twk_submit_test(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(
+            username='test', password='password',email='james@yahoo.com.tw')
+
+    def test_details(self):
+        publish_data = { "question": "question1", "stdin": "1 1", "stdout":"2", "language_id":"4" }
+        request = self.factory.post('/publish_hw/', json.dumps(publish_data), content_type='application/json')
+
+        request.user = self.user
+        save_publish_hw(request)
+
+        request = self.factory.get('/publish_hw/1/')
+        request.user = self.user
+        response = load_publish_hw(request, 1)
+
+        self.assertEqual(response.status_code, 200)
+
+        request = self.factory.get('publish_hw/')
+        request.user = self.user
+        response = save_publish_hw(request)
+        
+        self.assertEqual(response.status_code, 200)
+        
--- twk_backend/twk_send/tests.py
+++ twk_backend/twk_send/tests.py
@@ -1,9 +1,1 @@
-from django.contrib.auth.models import AnonymousUser, User
-from django.test import RequestFactory, TestCase, Client
-
-from twk_auth.views import login
-
-from twk_load.views import load_hw
-
-from twk_save.views import save_hw
-
+from django.test import TestCase
