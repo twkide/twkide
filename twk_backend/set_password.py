a1ae758629fdf864b8c4cd23d1110b85536cbcf2
何政剛 <james0828@ezmeals.tw>
2018-11-09 01:21:53 +0800

    in set_password.py create admin user

--- twk_backend/set_password.py
+++ twk_backend/set_password.py
@@ -5,3 +5,9 @@ for user in User.objects.all():
     user.set_password(user.password)
     user.save()
 
+user = User.objects.create(username='admin', password='password')
+user.set_password(user.password)
+user.is_superuser=True
+user.is_staff=True
+user.save()
+
